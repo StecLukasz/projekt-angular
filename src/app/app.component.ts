f2511231543818488011ee8fcbc3f33eb7f6a9be
StecLukasz <lukaszstec14@gmail.com>
2022-07-21 14:30:43 +0200

    nauki ciag dalszy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'projekt-angular';
+  title = 'projekt angular';
+
+
+  days = ['Poniedziałek', 'Wtorek','Środa','Czwartek','Piątek','Sobota','Niedziela'];
+
+  dogs = new Array<Dog>();
+  constructor(){
+    this.dogs.push(new Dog('Kazan' , 13), new Dog('Puszek', 4), new Dog('Kupa', 8) , new Dog('Pimpek', 6), new Dog('Sara', 1));
+  }
+ isHidden = false;
+
+ hidden(){
+this.isHidden = !this.isHidden;
+ }
 }
+
+class Dog{
+  constructor(public name: string, public age: number){
+  }
+}
+
